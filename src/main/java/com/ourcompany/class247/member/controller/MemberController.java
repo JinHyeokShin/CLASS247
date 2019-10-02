0c58339bb1891a055de96f19ed72910d6399b6f7
JinHyeokShin <43067184+JinHyeokShin@users.noreply.github.com>
2019-10-02 14:16:03 +0900

    pageInfo/pagination/@aunotation(?)

--- src/main/java/com/ourcompany/class247/member/controller/MemberController.java
+++ src/main/java/com/ourcompany/class247/member/controller/MemberController.java
@@ -1,5 +1,8 @@
 package com.ourcompany.class247.member.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MemberController {
 
 }
--- src/main/java/com/ourcompany/class247/member/model/dao/MemberDao.java
+++ src/main/java/com/ourcompany/class247/member/model/dao/MemberDao.java
@@ -1,5 +1,8 @@
 package com.ourcompany.class247.member.model.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository("mDao")
 public class MemberDao {
 
 }
