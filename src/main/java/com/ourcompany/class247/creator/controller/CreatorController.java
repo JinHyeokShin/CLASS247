2fd5a9daec3b34077a1708c85374984521f08e1c
may-h <hms9210@naver.com>
2019-10-12 23:38:13 +0900

    20191012-2338

--- src/main/java/com/ourcompany/class247/creator/controller/CreatorController.java
+++ src/main/java/com/ourcompany/class247/creator/controller/CreatorController.java
@@ -42,7 +42,7 @@ public class CreatorController {
 			model.addAttribute("creator", creator);
 			return "creator/creatorCenter";
 		} else {
-			return "creator/test";
+			return "creator/noCreatorPage";
 		}
 		
 		
