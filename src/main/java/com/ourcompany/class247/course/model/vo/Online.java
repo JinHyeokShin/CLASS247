39ed6fbb2419a7ec25361cf999dc6a8d7ba43fde
ksm_academy <wktndudtls@gmail.com>
2019-10-15 10:43:13 +0900

    vo 1015_1042

--- src/main/java/com/ourcompany/class247/course/model/vo/Online.java
+++ src/main/java/com/ourcompany/class247/course/model/vo/Online.java
@@ -24,11 +24,11 @@ public class Online extends Course{
 		CourseVideoUrl = courseVideoUrl;
 	}
 
-	public Online(int courseNum, String categoryName, int memNum, String courseTitle, String courseContent,
+	public Online(int courseNum, String categoryName, int creNum, String courseTitle, String courseContent,
 			String courseKind, int loveCount, int courseCurrentNum, Date courseEnrollDate, Date courseApprovedDate,
 			Date courseStartDate, Date courseEndDate, String courseApproved, String courseStatus, int onlineNum, int coursePrice, String courseMaterial, int courseMaterialPrice,
 			String courseVideoUrl) {
-		super(courseNum, categoryName, memNum, courseTitle, courseContent,
+		super(courseNum, categoryName, creNum, courseTitle, courseContent,
 				courseKind, loveCount, courseCurrentNum, courseEnrollDate, courseApprovedDate,
 				courseStartDate, courseEndDate, courseApproved, courseStatus);
 		this.onlineNum = onlineNum;
